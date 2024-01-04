c00b52d5807f3a14bc580c617bfcf4d8c6aed43f
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2024-01-04 09:50:52 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+// src/app/app.module.ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CurrencyService } from './currency.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [CurrencyService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
